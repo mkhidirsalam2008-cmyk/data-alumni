1f28ade63f3a25a4cbfd5481794e66840fd7b41b
mkhidirsalam2008-cmyk <mkhidirsalam2008@gmail.com>
2026-06-11 16:16:56 +0700

    Update login design

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,9 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-              'name' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('123456'),
-        ]);
-    }
+{
+    $this->call([
+        AdminSeeder::class,
+    ]);
+}
 }
